81db31025754cd791dfc278e147bd4ebbe989b9c
zhouwentong <zhouwentong1993@gmail.com>
2017-07-23 22:16:36 +0800

    test

--- Demo1.py
+++ Demo1.py
@@ -1,16 +1,14 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
+# 监督分类
 #  X 是特征变量
-X = np.array([[-1,-1],[-2,9],[-3,-2],[1,1],[2,1],[-0.8,-1]])
+X = np.array([[-1, -1], [-2, 9], [-3, -2], [1, 1], [2, 1], [-0.8, -1]])
 # Y 是标签
-Y = np.array([1,1,1,2,2,2])
+Y = np.array([1, 1, 1, 2, 2, 2])
 clf = GaussianNB()
 # 训练
-clf.fit(X,Y)
+clf.fit(X, Y)
 GaussianNB()
 # 得到预测结果
-print(clf.predict([-0.8,-1]))
-
-
-
+print(clf.predict([-0.8, -1]))
---
+++ Demo2.py
@@ -0,0 +1,21 @@
+from numpy import *
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1, 1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+def classify0(inX,dataSet,labels,K):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX,(dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(K):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems()),
+        key = operator.itemgetter

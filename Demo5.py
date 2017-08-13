c33b42cda8384e31c82d5d25f44bc01b98c8dfce
zhouwentong <zhouwentong1993@gmail.com>
2017-08-13 20:12:57 +0800

    自定义流水线，通过欧氏距离实现。 通过比对坐标系的每一个点距离，求出最短距离（待研究）

--- Demo5.py
+++ Demo5.py
@@ -1,8 +1,44 @@
+import random
+
+
+class ScrappyKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = self.closest(row)
+            predictions.append(label)
+
+        return predictions
+
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+
+        for i in range(1, len(self.X_train)):
+            dist = euc(row,self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+
+        return self.y_train[best_index]
+
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
+
+
 from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 from sklearn import tree
-from sklearn.neighbors import KNeighborsClassifier
+from scipy.spatial import distance
+
+# from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
 
@@ -11,13 +47,10 @@ Y = iris.target
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=.5)
 
-my_classifier = KNeighborsClassifier()
-
+my_classifier = ScrappyKNN()
 
 my_classifier.fit(x_train, y_train)
 
 predictions = my_classifier.predict(x_test)
 
-print(accuracy_score(y_test,predictions))
-
-
+print(accuracy_score(y_test, predictions))

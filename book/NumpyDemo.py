b3740b85c8f0bc3699760871f45b88a8d2c27ea2
zhouwentong <zhouwentong1993@gmail.com>
2017-08-20 10:31:05 +0800

    Numpy 基本功能 k-临近算法一点点

---
+++ book/NumpyDemo.py
@@ -0,0 +1,11 @@
+from numpy import *
+# print(random.rand(4, 4))
+
+# mat 是生成一个矩阵
+# .I 是求逆矩阵
+# 矩阵相乘直接使用 * 就行
+# eye 代表单位矩阵，通过传入的维度判断是 n 维
+m = mat(random.rand(4, 4))
+print(m.I * m)
+
+print(eye(4))
